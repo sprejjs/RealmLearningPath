c4c517133f56013548904d2362d910dde262bdcf
Allan Spreys <allan@spreys.com>
2023-04-22 10:32:39 +0100

    Adding the empty project

---
+++ ToDoWithRealm/ToDoWithRealm/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDoWithRealm
+//
+//  Created by Allan Spreys on 22/04/2023.
+//
+
+import UIKit
+final class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemRed
+  }
+}
+
